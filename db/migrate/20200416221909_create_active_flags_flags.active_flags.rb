74fb5e3ad817c9ef08a7ea5a663b74e5a2198b97
Nathan Huberty <nathan.huberty@orange.fr>
2020-04-17 01:12:55 +0200

    Add uuid to existing tables.

--- db/migrate/20200416221909_create_active_flags_flags.active_flags.rb
+++ db/migrate/20200416221909_create_active_flags_flags.active_flags.rb
@@ -1,7 +1,7 @@
 # This migration comes from active_flags (originally 20190304151043)
 class CreateActiveFlagsFlags < ActiveRecord::Migration[6.0]
   def change
-    create_table :active_flags_flags do |t|
+    create_table :active_flags_flags, id: :uuid do |t|
       t.string :key
       t.string :value
       t.integer :subject_id
