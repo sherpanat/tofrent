c5cf33187d334f80e8e91dafe7b92afc12c077b9
Nathan Huberty <nathan.huberty@orange.fr>
2020-04-22 01:11:56 +0200

    Add friendship on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :recoverable, :rememberable,
          :validatable, :trackable, :lockable, :confirmable
   
+  has_friendship
   has_many :torrents
 end
