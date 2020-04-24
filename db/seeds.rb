b669978efcf759267137937fb4ef5df3e540f56e
Nathan Huberty <nathan.huberty@orange.fr>
2020-04-24 20:18:00 +0200

    End seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Torrent.destroy_all
+User.destroy_all
+
+puts "Create 6 users with..."
+6.times do
+  user = User.create!(
+    email: Faker::Internet.email,
+    password: "12345678",
+    nickname: Faker::JapaneseMedia::DragonBall.character
+  )
+
+  p "with 2 torrents..."
+  2.times do
+    Torrent.create!(
+      owner: user,
+      source: :handmade,
+      title: Faker::Book.title,
+      description: Faker::Lorem.paragraphs(number: rand(1..3)),
+      visibility: :friends
+    )
+  end
+end
+p "done."
+
+puts "Create friendships..."
+User.all.each do |user|
+  users = User.where.not(id: user.id)
+  3.times do
+    friend = users.sample
+    unless user.friends_with?(friend)
+      user.friend_request(friend)
+      friend.accept_request(user)
+    end
+    users = users.where.not(id: friend.id)
+  end
+end
+p "done."
