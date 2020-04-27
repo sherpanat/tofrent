9c6002651a1081db20b948a52795ba3ccace3207
Nathan Huberty <nathan.huberty@orange.fr>
2020-04-27 23:20:58 +0200

    Reorganise scss packs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,8 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery");
 require("bootstrap");
-
-import "./application.scss";
+import "../stylesheets/application.scss";
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
