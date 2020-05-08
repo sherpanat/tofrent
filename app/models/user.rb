950f959d89a6d6fa0ebf17b6787f4fcb0f279de7
Nathan Huberty <nathan.huberty@orange.fr>
2020-05-08 22:33:14 +0200

    Add nickname in sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,18 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :timeoutable and :omniauthable
+  MINIMUM_NICKNAME_LENGTH = 4
   devise :database_authenticatable, :recoverable, :rememberable,
          :validatable, :trackable, :lockable, :confirmable, :registerable
   
   has_friendship
   has_many :torrents
-  
+
+  validates :nickname, presence: true, uniqueness: true, length: { minimum: MINIMUM_NICKNAME_LENGTH }
   validate :membership_validity
+
+
+  private
 
   def membership_validity
     unless RegistrationMembership.accepted.find_by_email(email)
