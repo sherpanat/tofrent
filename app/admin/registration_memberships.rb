8140901a1e502c04133a4018264d30ff9452afff
Nathan Huberty <nathan.huberty@orange.fr>
2020-05-08 19:00:42 +0200

    Rename admin from batcave to fortress_of_solitude.

--- app/admin/registration_memberships.rb
+++ app/admin/registration_memberships.rb
@@ -7,22 +7,22 @@ ActiveAdmin.register RegistrationMembership do
   end
 
   action_item :accept_membership, only: :show, if: proc{ resource.pending? || resource.refused? } do
-    link_to 'Accepter', accept_membership_batcave_registration_membership_path(resource), method: :put
+    link_to 'Accepter', accept_membership_fortress_of_solitude_registration_membership_path(resource), method: :put
   end
 
   action_item :refuse_membership, only: :show, if: proc{ resource.pending? || resource.accepted? } do
-    link_to 'Refuser', refuse_membership_batcave_registration_membership_path(resource), method: :put
+    link_to 'Refuser', refuse_membership_fortress_of_solitude_registration_membership_path(resource), method: :put
   end
 
   member_action :accept_membership, method: :put do
     resource.update!(status: :accepted)
-    redirect_to batcave_registration_membership_path(resource)
+    redirect_to fortress_of_solitude_registration_membership_path(resource)
     UserMailer.with(email: resource.email).membership_accepted.deliver_now
   end
 
   member_action :refuse_membership, method: :put do
     resource.update!(status: :refused)
-    redirect_to batcave_registration_membership_path(resource)
+    redirect_to fortress_of_solitude_registration_membership_path(resource)
     UserMailer.with(email: resource.email).membership_refused.deliver_now
   end
 end
