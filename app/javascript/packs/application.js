5553e6cab0f08a457230a4898a94d4e88da8aa8e
Nathan Huberty <nathan.huberty@orange.fr>
2020-06-07 01:22:37 +0200

    Add and setup stimulus

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,3 +19,11 @@ import "../stylesheets/application.scss";
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+// load stimulus
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("../controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
